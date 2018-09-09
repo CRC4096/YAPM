016f5f3aebe82f7ba93abbef145e7df4c476eea2
CRC4096 <ryanmcflames@gmail.com>
2018-09-09 12:06:54 +0200

    Debugging build path

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -3,7 +3,7 @@
 set -ex
 
 apt-get update
-apt-get install -y libstdc++-8-dev clang-6.0 qt5-default qtquickcontrols2-5-dev qtdeclarative5-dev
+apt-get install -y clang-6.0 qt5-default qtquickcontrols2-5-dev qtdeclarative5-dev
 
 if [[ "${CXX}" == clang* ]]
 then
