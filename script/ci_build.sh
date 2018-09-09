46c9dac9eba4ca6b34cdb59adf8420d0aa63fe7d
CRC4096 <ryanmcflames@gmail.com>
2018-09-09 11:35:20 +0200

    Debugging build path

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -3,13 +3,13 @@
 set -ex
 
 apt-get update
-apt-get install -y libstdc++-8-dev clang-6.0 qt5-default qtquickcontrols2-5-dev
+apt-get install -y libstdc++-8-dev clang-6.0 qt5-default qtquickcontrols2-5-dev qtdeclarative5-dev
 
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
 fi
-ls -la
+
 cmake -H. -BBuild -DCMAKE_BUILD_TYPE=RELEASE -Wdev
 cd Build
 make -j 2
